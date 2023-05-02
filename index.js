6a6635a0da6ac41616dd7f4722b50fd27a72b8fe
anoushk1234 <anoushka1828@gmail.com>
2023-05-02 18:54:50 +0530

    api function

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const nodejs_rust = require("nodejs-rust");
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
 functions.http("run-function", (req, res) => {
-	console.log("Hello, world!");
-	const { a, b } = req.query;
-	res.send(`SUM: ${nodejs_rust.sum(a, b)}`);
+  console.log("Hello, world!");
+  const { a, b } = req.query;
+  res.send(`SUM: ${nodejs_rust.sum(a, b)}`);
+});
+
+functions.http("call-secret-api", (req, res) => {
+  const { org } = req.query;
+  const key = "secret-key"; // add secret logic here
+  const result = nodejs_rust.callSecretApi(key, org);
+
+  res.send(`Result: ${result}`);
 });
--- nodejs-rust/src/lib.rs
+++ nodejs-rust/src/lib.rs
@@ -1,4 +1,5 @@
 #![deny(clippy::all)]
+use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
 
 #[macro_use]
 extern crate napi_derive;
@@ -6,4 +7,20 @@ extern crate napi_derive;
 #[napi]
 pub fn sum(a: i32, b: i32) -> i32 {
   a + b
+}
+
+#[napi]
+pub async fn call_secret_api(key: String, org: String) -> String {
+  let client = reqwest::Client::new();
+  let resp = client
+    .get(format!("https://api.github.com/orgs/{}/repos", org))
+    .header(CONTENT_TYPE, "application/json")
+    .header(AUTHORIZATION, format!("Bearer {}", key))
+    .send()
+    .await
+    .unwrap()
+    .text()
+    .await
+    .unwrap();
+  resp
 }
