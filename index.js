f02d1157b8ba27baa848c8aa0f2ad4964e4c57df
anoushk1234 <anoushka1828@gmail.com>
2023-05-03 12:14:12 +0530

    log

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const functions = require("@google-cloud/functions-framework");
 const nodejs_rust = require("nodejs-rust");
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
+console.log("starting functions...");
 functions.http("run-function", (req, res) => {
   console.log("Hello, world!");
   const { a, b } = req.query;
